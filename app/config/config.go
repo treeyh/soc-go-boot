ad0af24239181b6d4df0e52abe3da5af7b7d3522
tree <tree@ejyi.com>
2019-12-17 23:33:09 +0800

    提交代码

--- app/config/config.go
+++ app/config/config.go
@@ -5,43 +5,24 @@ import (
 	"github.com/spf13/viper"
 )
 
-var conf Config = Config{
+var conf SocConfig = SocConfig{
 	Viper:         viper.New(),
 	Mysql:         nil,
 	Redis:         nil,
-	Mail:          nil,
-	Server:        nil,
-	DingTalk:      nil,
-	FeiShu:        nil,
 	ScheduleTime:  nil,
 	Logs:          nil,
 	Application:   nil,
-	Parameters:    nil,
-	Mq:            nil,
-	OSS:           nil,
 	ElasticSearch: nil,
-	Sentry:        nil,
-	SMS:           nil,
 }
 
 type SocConfig struct {
 	Viper         *viper.Viper
 	Mysql         *MysqlConfig        //数据库配置
 	Redis         *RedisConfig        //redis配置
-	Mail          *MailConfig         //邮件配置
-	Server        *ServerConfig       //服务配置
-	DingTalk      *DingTalkSDKConfig  //钉钉配置
-	FeiShu        *FeiShuSdkConfig    //飞书配置
 	ScheduleTime  *ScheduleTimeConfig //定时时间配置
 	Logs          *map[string]LogConfig
 	Application   *ApplicationConfig   //应用配置
-	Parameters    *ParameterConfig     //参数配置
-	Mq            *MQConfig            //mq配置
-	OSS           *OSSConfig           //oss配置
 	ElasticSearch *ElasticSearchConfig //es配置
-	Sentry        *SentryConfig        //sentry配置
-	SkyWalking    *SkyWalkingConfig    //skywalking配置
-	SMS           *SMSConfig           //消息配置
 }
 
 func loadExtraConfig(dir string, config string, env string, extraConfig interface{}) error {
