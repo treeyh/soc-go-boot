847b5278c1e43e968d8a0f4cb47b06e0a94d9e74
tree <tree@ejyi.com>
2021-09-22 18:53:12 +0800

    调整异常捕获策略

--- route/middleware/exception/exception.go
+++ route/middleware/exception/exception.go
@@ -1,10 +1,14 @@
 package exception
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/treeyh/soc-go-boot/model/resp"
+	"github.com/treeyh/soc-go-common/core/errors"
 	"github.com/treeyh/soc-go-common/core/logger"
 	"runtime/debug"
 	"strings"
+	"time"
 )
 
 var (
@@ -15,14 +19,20 @@ func SetUp() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		defer func() {
 			if err := recover(); err != nil {
-				DebugStack := ""
+				DebugStack := fmt.Sprintf("%v \t", err)
 				for _, v := range strings.Split(string(debug.Stack()), "\n") {
 					DebugStack += v + ";"
 				}
 
 				log.ErrorCtx(c.Request.Context(), DebugStack)
-				//utilGin := req.GinContext{Ctx: c}
-				//controller.Json(&utilGin, 503, errors.ServerError.Code(), errors.ServerError.Message()+";"+DebugStack, nil)
+
+				c.JSON(200, resp.RespResult{
+					Code:      errors.SystemErr.Code(),
+					Message:   errors.SystemErr.Message(),
+					Timestamp: time.Now().Unix(),
+				})
+				c.Abort()
+				return
 			}
 		}()
 		c.Next()
